bb67f2526919979d7cda5cb99c5210b22a23501a
andymarsden <46504022+andymarsden@users.noreply.github.com>
2026-04-30 13:45:35 +0000

    routing sorted out

--- src/routes/(web)/+page.svelte
+++ src/routes/(web)/+page.svelte
@@ -1,4 +1,4 @@
 <div class="flex flex-1 flex-col gap-4 px-4 py-10">
-	<div class="bg-muted/50 mx-auto h-24 w-full max-w-3xl rounded-xl"></div>
+	<div class="bg-muted/50 mx-auto h-24 w-full max-w-3xl rounded-xl">WEBSITE</div>
 	<div class="bg-muted/50 mx-auto h-full w-full max-w-3xl rounded-xl"></div>
 </div>
